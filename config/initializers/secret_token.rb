fb9bf385c377598a88e4141a96efc417c9cf0931
Tom Davies <t0mdavies@live.co.uk>
2013-03-15 23:25:27 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ultra::Application.config.secret_token = '34983e902adb0b387c20b90295ab6207ff4a4a76cef18a8a13314f3653b6a2e885539d82334221ae4c92f1f06e1b14d585588a63d2979295a22d4710643ce7e8'
